dca2a0693a984093b5de790c7f7cedda3a74a67b
valliyappan-18 <valliyappan02@gmail.com>
2024-04-29 15:45:30 +0530

    script

--- src/script.js
+++ src/script.js
@@ -7,12 +7,6 @@ const header = document.querySelector('#header');
 
 const nav_link = document.querySelectorAll('.nav-link');
 
-nav_link.forEach((link)=>{
-    navitem.addEventListener('click',()=>{
-        navitem.classList.toggle('hidden');
-    })
-})
-
 menu.addEventListener('click',()=>{
     navitem.classList.remove('hidden');
     section.style.opacity = 0.2;
